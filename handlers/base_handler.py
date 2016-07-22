cd4456f63b97d6db05692a095f380b0fad74055b
whoami <19941222hb@gmail.com>
2016-07-22 23:29:22 +0800

    optimize code

--- handlers/base_handler.py
+++ handlers/base_handler.py
@@ -3,25 +3,22 @@
 from tornado import web, gen
 
 
-class AuthNeedBaseHandler(web.RequestHandler):
+class BaseHandler(web.RequestHandler):
+
+    def write_error(self, status_code, **kw):
+        self.render("errors/" + str(status_code) + '.html')
+
+class AuthNeedBaseHandler(BaseHandler):
 
     @gen.coroutine
     def prepare(self):
-        session_id = self.get_secure_cookie(
-            self.application.settings['session_cookie'])
+        session_id = self.get_secure_cookie(self.application.settings['session_cookie'],None)
         self.session = self.application.session_cache.get_session()
         yield self.session.start(session_id)
         self.current_user = self.session.all()
-
-    def write_error(self, status_code, **kw):
-        self.render('errors/' + str(status_code) + '.html')
 
     def on_finish(self):
         if hasattr(self, 'session'):
             self.session.cache()
 
 
-class BaseHandler(web.RequestHandler):
-
-    def write_error(self, status_code, **kw):
-        self.render("errors/" + str(status_code) + '.html')
