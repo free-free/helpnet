89d5304ff2c9a3441b208cc93fef0b121c4e3483
whoami <19941222hb@gmail.com>
2016-08-02 15:36:49 +0800

    make a code change

--- handlers/callback_handlers.py
+++ handlers/callback_handlers.py
@@ -5,13 +5,30 @@ import xml.etree.ElementTree as etree
 #
 # weixin callback url handler
 class CallbackHandler(web.RequestHandler):
-    
+    r'''
+       Request Message keys:
+           ToUserName: receive user name
+           FromUserName: sending user name
+           CreateTime: timestamp
+           MsgType: 
+           Content:
+           MsgId: int64
+    '''
+
+    def get_request_msg(self,name,default=None):
+        if not hasattr(self,"__msg"):
+            self.__msg = etree.fromstring(self.request.body)
+        msg_val = self.__msg.findtext(name) or default
+        return msg_val
+
     @gen.coroutine
     def get(self):
         self.write("callback handler")
     
     @gen.coroutine
     def post(self):
-        root = etree.fromstring(self.request.body)
-        print(root.findtext("ToUserName"))
+        msg_type = self.get_request_msg("MsgType")
+        if msg_type == "text":
+            pass
+        
       
