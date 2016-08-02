3af0e5df0abc79587dfd6feae6c0bd807fc57a27
whoami <19941222hb@gmail.com>
2016-08-02 15:25:39 +0800

    Fix bug

--- handlers/check_signature_handlers.py
+++ handlers/check_signature_handlers.py
@@ -8,7 +8,7 @@ class CheckSignatureHandler(web.RequestHandler):
 
     def get(self):
         signature = self.get_argument("signature","")
-        timestamp = self.get_arguemnt("timestampe","")
+        timestamp = self.get_argument("timestampe","")
         nonce = self.get_argument("nonce","")
         echostr = self.get_argument("echostr","")
         param_list = [TOKEN,timestamp,nonce]
@@ -16,8 +16,8 @@ class CheckSignatureHandler(web.RequestHandler):
         param_str = ''.join(param_list).encode()
         sha1 = hashlib.sha1()
         sha1.update(param_str)
-        param_str = sha1.hexhdigest()
-        if signature == param_str.decode():
+        param_str = sha1.hexdigest()
+        if signature == param_str:
             self.write(echostr)
         else:
             self.write("")
