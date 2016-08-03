e8c118208ece0d91b2673229c33be4e51ffb270b
John <19941222hb@gmail.com>
2016-08-03 14:02:43 +0800

    remove common_handlers.py

--- handlers/callback_handlers.py
+++ handlers/callback_handlers.py
@@ -6,12 +6,11 @@ import xml.etree.ElementTree as etree
 # weixin callback url handler
 class CallbackHandler(web.RequestHandler):
     r'''
-       Request Message keys:
+       Request Message common keys:
            ToUserName: receive user name
            FromUserName: sending user name
            CreateTime: timestamp
            MsgType: 
-           Content:
            MsgId: int64
     '''
 
@@ -21,6 +20,39 @@ class CallbackHandler(web.RequestHandler):
         msg_val = self.__msg.findtext(name) or default
         return msg_val
 
+    def handle_text(self):
+        r"""
+        request text msg keys:
+           Content
+        """
+        self.write(self.get_request_msg("MsgType"))
+
+    def handle_image(self):
+        r"""
+         request image msg keys:
+                PicUrl
+        """            
+        self.write(self.get_request_msg("MsgType"))
+
+    def handle_event(self):
+        self.write(self.get_request_msg("MsgType"))
+   
+    def handle_location(self):
+        r"""
+        request location msg keys:
+            Location_X
+            Location_Y
+            Scale
+            Label
+        """    
+        self.write(self.get_request_msg("MsgType"))
+
+    def handle_link(self):
+        self.write(self.get_request_msg("MsgType"))
+
+    def handle_other(self):
+        self.write("")
+
     @gen.coroutine
     def get(self):
         self.write("callback handler")
@@ -28,7 +60,6 @@ class CallbackHandler(web.RequestHandler):
     @gen.coroutine
     def post(self):
         msg_type = self.get_request_msg("MsgType")
-        if msg_type == "text":
-            pass
+        getattr(self,'handle_'+msg_type,'handle_other')()
         
       
