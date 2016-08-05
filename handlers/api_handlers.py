3241ca6e4f252f73b94fd4e816d2c3f2d9256178
John <19941222hb@gmail.com>
2016-08-05 22:34:12 +0800

    implement weixin qrcode api handler

---
+++ handlers/api_handlers.py
@@ -0,0 +1,41 @@
+# coding:utf-8
+
+from tornado import gen,web
+from .base_handler import BaseHandler
+import json
+from . import send_async_request
+
+
+class WeixinQRCodeAPIHandler(BaseHandler):
+
+    r"""
+        @url:/resources/WeixinQRCodeResource/get/?
+    """ 
+    @gen.coroutine
+    def get(self):
+        qrcode_ticket = yield self.application.cache.sget('weixin_qrcode_ticket')
+        if qrcode_ticket:
+            url = "https://mp.weixin.qq.com/cgi-bin/showqrcode?ticket="+qrcode_ticket
+            self.set_header("Cache-Control","no-cache")
+            self.write({"qrcode_url":url})
+        else:
+            token = yield self.application.cache.sget("weixin_api_token")
+            url = "https://api.weixin.qq.com/cgi-bin/qrcode/create?access_token="+token
+            body = {"expire_seconds": 86400, "action_name": "QR_SCENE", "action_info": {"scene": {"scene_id": 123}}}
+            body = json.dumps(body)
+            try:
+                response = yield send_async_request(url, method="POST", body=body)
+                response = response.body.decode("utf-8")
+                response = json.loads(response)
+                if "ticket" in response:
+                    url = "https://mp.weixin.qq.com/cgi-bin/showqrcode?ticket="+response["ticket"]
+                    yield self.application.cache.set("weixin_qrcode_ticket",response["ticket"])
+                    yield self.application.cache.expire("weixin_qrcode_ticket",response.get("expire_seconds",0))
+                    self.write({"qrcode_url":url})
+                else:
+                    self.set_header("Cache-Control","no-cache")
+                    self.write({"errmsg":"can't get qrcode","errcode":40002})
+            except Exception as e:
+                print(e)
+                self.write({"errmsg":"can't get qrcode","errcode":40001})
+            
