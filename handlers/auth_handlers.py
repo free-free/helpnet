5fbf5a2d25a0f86c620d024a9596eceef9149ef6
John <19941222hb@gmail.com>
2016-08-07 00:21:37 +0800

    add weixin authentication login handler

--- handlers/auth_handlers.py
+++ handlers/auth_handlers.py
@@ -4,13 +4,14 @@
 from tornado import web
 from tornado import gen
 from .base_handler import BaseHandler, AuthNeedBaseHandler
+from . import send_async_request
 from concurrent.futures import ThreadPoolExecutor
 import bcrypt
 from datetime import datetime
 import uuid
 import base64
 import json
-
+import time
 
 class LoginMixin(object):
 
@@ -58,6 +59,55 @@ class LoginHandler(AuthNeedBaseHandler, LoginMixin):
     def post(self):
         yield self.signin('/')
 
+
+class WXAuthCallbackLoginHandler(AuthNeedBaseHandler):
+    
+    r"""
+        @url:/wxauthlogin/?
+    """
+    @gen.coroutine
+    def prepare(self):
+        yield super(WXAuthCallbackLoginHandler,self).prepare()
+        if self.current_user:
+            self.redirect('/')
+   
+    @gen.coroutine
+    def get(self):
+        state = self.get_argument("state")
+	code = self.get_argument("code",None)
+        if not code:
+            # weixin reject to authentication,redirect to login page
+            self.redirect('/login')
+            return 
+        ackurl = "https://api.weixin.qq.com/sns/oauth2/access_token? \
+            appid={0}&secret={1}&code={2}&grant_type=authorization_code"
+        ackurl.format(APPID,SECRET,code)
+        try:
+            response = yield send_async_request(ackurl)
+            response = response.body.decode("utf-8")
+            openid = response['openid']
+            #check user existence
+            userdata = yield self.application.db['user'].find({"userid":openid})
+            if not userdata:
+                userdataurl = "https://api.weixin.qq.com/sns/userinfo?access_token={0}&openid={1}"
+                userdataurl.format(response['access_token'],openid)
+                response = yield send_async_request(userdataurl)
+                userdata = response.body.decode('utf-8') 
+                userdata['createtime'] = time.time()
+                userdata['subscribe'] = 0
+                userdata['userid'] = userdata.pop('openid')
+                userdata['username'] = userdata.pop('nickname')
+                del userdata['privilege']
+                yield self.application.db['user'].insert(userdata)
+            del userdata['_id']
+            self.session.multi_set(userdata)
+            yield self.session.save(7200)
+            self.set_cookie("auth",1)
+            self.set_cookie(self.application.settings['session_cookie'],self.session.session_id)
+            self.redirect('/')
+        except Exception:
+            self.redirect('/login')
+            self.finish()
 
 class LogoutHandler(AuthNeedBaseHandler):
     r"""
