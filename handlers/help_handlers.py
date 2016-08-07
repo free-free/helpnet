eb829cda29cf74e6ee53798a4d58ccef19d19eda
John <19941222hb@gmail.com>
2016-08-07 23:03:32 +0800

    forget fix what

--- handlers/help_handlers.py
+++ handlers/help_handlers.py
@@ -2,21 +2,21 @@
 
 
 from tornado import gen,web
-from .base_handler import BaseHandler
+from .base_handler import BaseHandler,AuthNeedBaseHandler
 import shortuuid
 import time
 
-class HelpListHandler(BaseHandler):
+class HelpListHandler(AuthNeedBaseHandler):
      
     r'''
      @url: /
     ''' 
     @gen.coroutine
     def get(self):
-        self.render("map.html")
+        self.render("helplist.html")
 
 
-class PostHelpHandler(BaseHandler):
+class PostHelpHandler(AuthNeedBaseHandler):
    
     r"""
         @url:/askhelp
@@ -40,6 +40,8 @@ class PostHelpHandler(BaseHandler):
         helpinfo['location'] = []
         helpinfo['location'][0] = self.get_argument("longitude")
         helpinfo['location'][1] = self.get_argument("latitude")
+        helpinfo['address'] = self.get_argument("address",None) \
+            or self.current_user['address']
         helpinfo['locprec'] = 0.0
         helpinfo['post_username'] = self.current_user['username']
         helpinfo['post_userid'] = self.current_user['userid']
@@ -47,34 +49,68 @@ class PostHelpHandler(BaseHandler):
         helpcontact = self.get_argument("help_contact",None) \
             or self.current_user['usercontact']
         helpinfo['post_usercontact'] = helpcontact 
-        help_exist = yield self.application.db['help'].find_one({'helpid':helpinfo['helpid']},{"helpid":1})
-        if not help_exist:
-            response = yield self.application.db['help'].insert(helpinfo)
-            if response:
-                self.write("askhelp_success.html")
+        try:
+            help_exist = yield self.application.db['help'].find_one({'helpid':helpinfo['helpid']},{"helpid":1})
+            if not help_exist:
+                response = yield self.application.db['help'].insert(helpinfo)
+                if response:
+                    self.write("askhelp_success.html")
+                else:
+                    self.set_status(500)
+                    self.render("errors/500.html")
             else:
                 self.set_status(500)
                 self.render("errors/500.html")
-        else:
+        except Exception:
             self.set_status(500)
             self.render("errors/500.html")
-	
-       
-        
 
 
-class HelpItemDetailHandler(BaseHandler):
+class GetHelpHandler(AuthNeedBaseHandler):
+   
+    r"""
+        @url:/gethelp/([0-9a-zA-Z]+)/?
+    """
+    @gen.coroutine
+    def post(self,helpid):
+        try:
+            helpdata = yield self.application.db['help'].find_one({"helpid":helpid})
+            if not helpdata:
+                self.set_status(500)
+                self.render("errors/500.html")
+                return 
+            if helpdata['helpstate'] == 0:
+                helpdata['helpstate'] = 1
+                helpdata['get_userid'] = self.current_user['userid']
+                helpdata['get_username'] = self.current_user['username']
+                helpdata['get_userheadimgurl'] = self.current_user['headimgurl']
+                helpdata['finishtime'] = time.time()
+                self.render("gethelp_success.html") 
+            elif helpdata['helpstate'] == 1:
+                self.render("gethelp_failed.html",reason="already solved by others")
+            else:
+                self.render("gethelp_failed.html",readon="expired")
+        except Exception:
+            self.set_status(500)
+            self.render("errors/500.html")
+
+
+class HelpDetailHandler(AuthNeedBaseHandler):
 
     r"""
         @url:/help/([0-9a-zA-Z]+)/?
     """
     @gen.coroutine
-    def get(self, help_id):
-        help_info = yield self.application.db['help'].find_one({"helpid":help_id})
-        if help_info:
-            del help_info['_id']
-            self.write(help_info)
-        self.render("helpdetail.html")
-        #else:
-        #    self.set_status(404)
-        #    self.render("errors/404.html")
+    def get(self, helpid):
+        try:
+            helpdata = yield self.application.db['help'].find_one({"helpid":helpid})
+            if helpdata:
+                del helpdata['_id']
+                self.write(helpdata)
+            else:
+                self.render("helpdetail.html")
+                #self.set_status(404)
+                #self.render("errors/404.html")
+        except Exception:
+            self.set_status(500)
+            self.render("errors/500.html")
