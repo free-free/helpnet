fc3d0047969c6581a35a7aff545e672b297e5fab
John <19941222hb@gmail.com>
2016-08-08 12:18:09 +0800

    fully implement url help api handler

--- handlers/api_handlers.py
+++ handlers/api_handlers.py
@@ -6,7 +6,7 @@ import json
 from . import send_async_request
 
 
-class WeixinQRCodeAPIHandler(BaseHandler):
+class WeixinQRCodeGetAPIHandler(BaseHandler):
 
     r"""
         @url:/resource/WXQRCodeResource/get/?
@@ -41,7 +41,7 @@ class WeixinQRCodeAPIHandler(BaseHandler):
     
 
 
-class HelpContentAPIHandler(AuthNeedBaseHandler):
+class HelpContentGetAPIHandler(AuthNeedBaseHandler):
    
     r"""
         @url:/resource/HelpContentResource/get/?
@@ -51,30 +51,115 @@ class HelpContentAPIHandler(AuthNeedBaseHandler):
     def get(self):
         source_url = self.get_argument("source_url")
         data = self.get_argument("data",{})
-        longitude = data.get("longitude")
-        latitude = data.get("latitude")
-        offset = data.get("offset")
+        context = data.get("context")  
+        qrc = data.get("qsc")
+        yield getattr(context+'help',self.default)(qrc)
+   
+    @gen.coroutine
+    def updateshelp(self,qrc): 
+        longitude = qrc.get("lng")
+        latitude = qrc.get("lat")
+        last_help_pt = qrc.get("last_help_pt")
         if not longitude or not latitude:
             longitude = self.current_user['longitude']
             latitude = self.current_user['latitude']
         try:
-            cursor =  self.application.db['help'].find({  \
-                "location":{"$geoWithin":{"$center":[[longitude,latitude],0.1]}} })
-            cursor.sort("posttime",1).limit(10).skip(offset)
+            if last_help_pt <= 0:
+                query = {"location":{"$geoWithin":{"$center":[[longitude,latitude],0.1]}}}
+            else:
+                query = {"location":{"$geoWithin":{"$center":[[longitude,latitude],0.1]}},"posttime":{"$lt":last_help_pt}}
+            cursor = self.application.db['help'].find(query)
+            cursor.sort("posttime",1).limit(10)
             helpdata=[]
             while (yield cursor.fetch_next):
                 dataline = curosr.next_object()
                 del dataline['_id']
                 helpdata.append(dataline)
+            qrc["last_help_pt"] = helpdata[-1].get("posttime",0)
             response = {}
-            response['data']={}
-            response['data']['offset'] = offset+10
-            response['data']['helpdata'] = helpdata
+            response['res_qrc'] = qrc
+            response['data'] = helpdata
             self.write(response)
         except Exception:
             self.write({"errcode":40002,"errmsg":"no data"})
-           
-            
+
+    @gen.coroutine
+    def uposthelp(self,qrc):
+        last_help_pt = qrc.get("last_help_pt")
+        try:
+            userid = self.current_user["userid"]
+            if last_help_pt <= 0:
+                query = {"post_userid":userid}
+            else:
+                query = {"post_userid":userid,"posttime":{"$lt":last_help_pt}}
+            cursor = self.application.db['help'].find(query)
+            cursor.sort("posttime",1).limit(10)
+            helpdata = []
+            while (yield cursor.fetch_next):
+                dataline = cursor.next_object()
+                del dataline['_id']
+                helpdata.append(dataline)
+            qrc["last_help_pt"] = helpdata[-1].get('posttime',0)
+            response  = {}
+            response['res_qrc'] = qrc
+            response['data'] = posthelpdata
+            self.write(response)
+        except Exception:
+            self.write({"errcode":40003,"errmsg":" get post help error"})
+    
+    @gen.coroutine
+    def ugethelp(self,qrc):
+        last_help_pt = qrc.get("last_help_pt")
+        try:
+            userid = self.current_user['userid'] 
+            if last_help_pt <= 0:
+                query = {"get_userid":userid}
+            else:
+                query = {"get_userid":userid,"posttime":{"$lt":last_help_pt}}
+            cursor = self.application.db['help'].find(query)
+            cusor.sort("posttime",1).limit(10)
+            helpdata = []
+            while (yield cursor.fetch_next):
+                dataline = cursor.next_object()
+                del dataline['_id'] 
+                helpdata.append(dataline)
+            qrc['last_help_pt'] = helpdata[-1].get("posttime",0)
+            response = {}
+            response['res_qrc'] = qrc
+            response['data'] = helpdata
+            self.write(response)
+        except Exception:
+            self.write({"errcode":40004,"errmsg": "get help error"})
+
+    @gen.coroutine
+    def default(self,qrc):
+        response = {}
+        response['res_qrc'] = qrc
+        response['data'] = []
+        self.write(response)
+
+
+
+class UserProfileGetAPIHandler(AuthNeedBaseHandler):
+   
+    r"""
+        @url:/resource/UserProfileResource/get/?
+    """
+    @web.authenticated
+    @gen.coroutine
+    def get(self):
+        pass
+
+
+class UserProfileUpdateAPIHandler(AuthNeedBaseHandler):
+
+    r"""
+        @url:/resource/UserProfileResource/update/?
+    """
+    @web.authenticated
+    @gen.coroutine
+    def post(self):
+        pass    
       
         
                 
