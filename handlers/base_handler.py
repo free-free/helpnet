78a65ca782a1b4bc01b659128243d85326a640ba
John <19941222hb@gmail.com>
2016-08-21 21:46:52 +0800

    rewrite AuthNeedBaseHandler

--- handlers/base_handler.py
+++ handlers/base_handler.py
@@ -15,21 +15,24 @@ class BaseHandler(web.RequestHandler):
         if not self.get_cookie('auth'):
             self.set_cookie("auth",'0')
 
+
 class AuthNeedBaseHandler(BaseHandler):
 
     @gen.coroutine
     def prepare(self):
         super(AuthNeedBaseHandler,self).prepare()
+        session_id = self.get_secure_cookie(self.application.settings['session_cookie'],None)
+        self.session = self.application.session_cache.get_session()
+        yield self.session.start(session_id)
         if self.get_cookie("auth",'0') == '0':
             self.current_user = {}
         else:
-            session_id = self.get_secure_cookie(self.application.settings['session_cookie'],None)
-            self.session = self.application.session_cache.get_session()
-            yield self.session.start(session_id)
             self.current_user = self.session.all()
 
+    @gen.coroutine
     def on_finish(self):
         if hasattr(self, 'session'):
+            yield self.session.end()
             self.session.cache()
 
 
