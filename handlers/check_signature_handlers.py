b0fccd742bbc6ab37271e7b9d9565e33926f46b7
John <19941222hb@gmail.com>
2016-08-21 22:14:40 +0800

    change token

--- handlers/check_signature_handlers.py
+++ handlers/check_signature_handlers.py
@@ -3,12 +3,12 @@
 from tornado import web
 import hashlib
 
-TOKEN = "JDEJOIDJEOJDEIOE"
+TOKEN = "huzhugctoken"
 class CheckSignatureHandler(web.RequestHandler):
 
     def get(self):
         signature = self.get_argument("signature","")
-        timestamp = self.get_argument("timestampe","")
+        timestamp = self.get_argument("timestamp","")
         nonce = self.get_argument("nonce","")
         echostr = self.get_argument("echostr","")
         param_list = [TOKEN,timestamp,nonce]
