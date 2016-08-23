66b84a1a53ef65696c9f4aa6a0682c342c4ce935
whoami <19941222hb@gmail.com>
2016-08-23 22:51:32 +0800

    change tasker config file

--- tasker.py
+++ tasker.py
@@ -2,5 +2,8 @@
 
 from celery import Celery
 
-tasker = Celery("tasker", broker="redis://localhost:6379/1")
+tasker = Celery("tasker", broker="redis://localhost:6379/1", include=['tasks'])
+tasker.config_from_object("celeryconfig")
+
+
 
