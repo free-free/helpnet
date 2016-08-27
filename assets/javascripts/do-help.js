0eab1a6b9796e292d3d6c533cc72d43909304ccc
john <19941222hb@gmail.com>
2016-08-27 21:17:58 +0800

    add dohelp page css style file and js file

---
+++ assets/javascripts/do-help.js
@@ -0,0 +1,28 @@
+
+function load_create_dohelp_data(){
+    var 
+}
+function check_mobile_number(target){
+
+}
+
+function create_dohelp_ui(parent, data){
+    
+    var helpDetail = document.createElement("div");
+    var helpDetailContainer = document.createElement("div");
+    helpDetail.className = "help_detail";
+    helpDetailContainer.className = "help_detail_container";
+    
+    var helpDetailHead = document.createElement("div");
+    var postUserHeadImg = document.createElement("img");
+    var postUserName = document.createElement("span");
+    helpDetailHead.className = "help_detail_head"
+    postUserHeadImg.className = "post_user_head_img user_head_img";
+    postUserName.className = "post_user_name user_name"
+
+    var helpInfo = document.createElement("div");
+    var require = document.createElement("h3");
+    var helpMainContent = document.createElement("p");    
+    helpInfo.className = "help_info";
+    require.className = "
+}
