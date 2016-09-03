e5579dd76dd24824eb5160b6b34eda3543162608
john <19941222hb@gmail.com>
2016-09-03 22:08:48 +0800

    write do help page

--- assets/javascripts/do-help.js
+++ assets/javascripts/do-help.js
@@ -1,6 +1,5 @@
 
 function load_create_dohelp_data(){
-    var 
 }
 function check_mobile_number(target){
 
@@ -8,21 +7,42 @@ function check_mobile_number(target){
 
 function create_dohelp_ui(parent, data){
     
-    var helpDetail = document.createElement("div");
-    var helpDetailContainer = document.createElement("div");
-    helpDetail.className = "help_detail";
-    helpDetailContainer.className = "help_detail_container";
+    var doHelpContainer = document.createElement("div");
+    doHelpContainer.className = "do_help_container";
     
-    var helpDetailHead = document.createElement("div");
+    var doHelpHead = document.createElement("div");
     var postUserHeadImg = document.createElement("img");
     var postUserName = document.createElement("span");
-    helpDetailHead.className = "help_detail_head"
+    doHelpHead.className = "do_help_head"
     postUserHeadImg.className = "post_user_head_img user_head_img";
-    postUserName.className = "post_user_name user_name"
+    postUserName.className = "post_username user_name"
+    doHelpHead.appendChild(postUserHeadImg);
+    doHelpHead.appendChild(postUserName);
+    
 
-    var helpInfo = document.createElement("div");
-    var require = document.createElement("h3");
-    var helpMainContent = document.createElement("p");    
+    var doHelpBody = document.createElement("div");
+    var helpContentTag= document.createElement("span");
+    var helContent = document.createElement("p");    
+    var postUserContactTag= document.createElement("span");
+    var postUserContact = document.createElement("p");    
+    var helpPrice = document.createElement("p");    
+    var doHelpForm = document.createElement("form");
+    var doHelpFromContainer = document.createElement("div");
+    var doUserContactMeans = document.createElement("select");
+
     helpInfo.className = "help_info";
-    require.className = "
+    require.className = "";
 }
+
+
+$(function(){
+   init_p2r();
+   $("#do_help_form").submit(function(){
+       var doUserContact = document.getElementById("do_usercontact"); 
+       var dohelpBtn = document.getElementById("dohelp_btn");
+       if(doUserContact.value == ""||doUserContact.value== "undefined"){
+           $.toptip("请留下你的联系方式","error");
+           return false;
+       }
+   }); 
+});
