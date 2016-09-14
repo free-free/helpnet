67eec84231590597964ced82cd7fbe7a4679c0e3
john <19941222hb@gmail.com>
2016-09-14 18:57:28 +0800

    add create_wx_menu()

--- tasks.py
+++ tasks.py
@@ -1,6 +1,12 @@
 # coding=utf-8
 
+import time
+import json
+
+import requests
+from redis   import Redis
 from submail import submail
+from motor import MotorClient
 
 from tasker import tasker
 from services import token_refresh, jsapi_ticket_refresh
@@ -24,4 +30,31 @@ def send_mail():
     mail['signature'] = "7e3faf32a3ca595bc3e81ff121bcad38"
     mail.send("xsend")
 
+@tasker.task
+def help_expire():
+    now_tm = time.time()
+    mongo = MotorClient("localhost", 4000)
+    help_order = mongo['fsp']['help_order']
+    criteria = {"expiretime":{"$lt":now_tm}}
+    modifier = {"state":2}
+    help_order.update(criteria, modifier)
+   
+@tasker.task
+def create_wx_menu():
+    redis = Redis("localhost", 6379)
+    access_token = redis.get("weixin_api_token")
+    access_token = access_token.decode()
+    url = "https://api.weixin.qq.com/cgi-bin/menu/create?access_token={0}"
+    url = url.format(access_token)
+    data = {
+        "button":[
+            {
+	        "type": "view",
+                "name": "寻求帮助",
+                "url": "http://www.huzhugc.com/wxpubloginredirect/"
+            },
+	]
+    }
+    req = requests.post(url, data=data)
+    return req.json()
 
