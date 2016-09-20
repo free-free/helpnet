c2e4acc980ae04a47a3e313b5c446c44aca329fd
john <19941222hb@gmail.com>
2016-09-20 22:05:47 +0800

    add send_help_solved_msg() task

--- tasks.py
+++ tasks.py
@@ -31,13 +31,44 @@ def send_mail():
     mail.send("xsend")
 
 @tasker.task
-def help_expire():
+def send_help_sovled_msg(uid, d_name, d_uct, d_uctm, content):
+    if ductm == "1":
+        d_uct += "(电话)"
+    elif d_uctm == "2":
+        d_uct += "(微信)"
+    else:
+        d_uct += "(QQ)"
+    req_param = {}
+    req_param['userid'] = userid
+    req_param['template_id'] = ""
+    req_param['url'] = "http://www.huzhugc.com/user/postedhelp/"
+    req_param['data'] = {}
+    req_param['data']['first']={"value":"","color":"#173177"}
+    req_param['data']['do_username'] = {"value":d_name, "color":"#173177"}
+    req_param['data']['do_usercontact'] = {"value":d_uct, "color":"#173177"}
+    req_param['data']['content'] = {"value":content, "color":"#173177"}
+    req_param['data']['remark'] = ""
+    redis = Redis("localhost", 6379)
+    access_token = redis.get("weixin_api_token")
+    access_token = access_token.decode()
+    req_url = "https://api.weixin.qq.com/cgi-bin/message/template/send?access_token={0}"
+    req_url = req_url.format(access_token)
+    req = requests.post(req_url , data=req_param)
+    resp = req.json()
+
+@tasker.task
+def update_help_expire():
     now_tm = time.time()
     mongo = MongoClient("localhost", 4000)
-    help_order = mongo['fsp']['help_order']
+    temp_help = mongo['hnet']['permanent_help']
+    updates_help = mongo['hnet']['updates_help']
     criteria = {"expiretime":{"$lt":now_tm}}
     modifier = {"$set":{"state":2}}
-    resp = help_order.update(criteria, modifier)
+    try:
+        resp1 = updates_help.remove(criteria, modifier)
+        resp2 = temp_help.update(criteria, modifier)
+    except Exception as e:
+        update_help_expire()
    
 @tasker.task
 def create_wx_menu():
