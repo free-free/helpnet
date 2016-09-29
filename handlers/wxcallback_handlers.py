0f119ddf2380e71dc56579344692c563eabfddcd
john <19941222hb@gmail.com>
2016-09-29 22:53:04 +0800

    check signature

--- handlers/wxcallback_handlers.py
+++ handlers/wxcallback_handlers.py
@@ -2,6 +2,7 @@
 
 import time
 import json
+import hashlib
 from datetime import datetime
 
 from tornado import gen,web
@@ -25,6 +26,19 @@ class WXCallbackHandler(BaseHandler):
 
     def prepare(self):
         super().prepare()
+        signature = self.get_argument("signature")
+        nonce = self.get_argument("nonce")
+        timestamp = self.get_argument("timestamp")
+        token = self.application.settings['wx_callback_token']
+        param_list = [token, timestamp, nonce]
+        param_list.sort()
+        param_str = ''.join(param_list).encode()
+        sha1 = hashlib.sha1()
+        sha1.update(param_str)
+        param_str = sha1.hexdigest()
+        if signature != param_str:
+            self.write("")
+            return 
         self.req_msg = xmltodict.parse(self.request.body)['xml']
 
     @gen.coroutine
@@ -33,8 +47,6 @@ class WXCallbackHandler(BaseHandler):
         request text msg keys:
            Content
         """
-        self.application.task.send_task('worker.send_mail')
-        print("send mail")
         self.write("")
  
     @gen.coroutine
@@ -107,45 +119,45 @@ class WXCallbackHandler(BaseHandler):
 
     @gen.coroutine
     def handle_subscribe_event(self):
-        user_openid = self.req_msg.get("FromUserName")
-        user_exists = yield self.application.db['user'].find_one({"userid":user_openid},{"userid":1})
-        if not user_exists:
-            try:
-                access_token = yield self.application.cache.sget('weixin_api_token')
-            except Exception as e:
-                self.write("")
-                return  
-            if access_token:
-                url = "https://api.weixin.qq.com/cgi-bin/user/info?access_token={0}&openid={1}&lang={2}"
-                url = url.format(access_token,user_openid,"zh_CN")
-                try:
-                    response = yield send_async_request(url)
-                    userdata = json.loads(response.body.decode("utf-8"))
-                    userdata['createtime'] = userdata.pop('subscribe_time',time.time()) 
-                    userdata['userid'] = userdata.pop('openid')
-                    userdata['username'] = userdata.pop('nickname')
-                    userdata['address'] = ''
-                    userdata['location'] = [0, 0]
-                    userdata['help_cnt'] = {"posted_help_num":0,"done_help_num":0}
-                    del userdata['remark']
-                    del userdata['groupid']
-                    del userdata['tagid_list']
-                    del userdata['language']
-                    yield self.application.db['user'].insert(userdata)
-                except Exception as e:
-                    pass
-                self.write("")  
-        else: 
-            self.write("") 
+        try:
+            user_openid = self.req_msg.get("FromUserName")
+            user_existence = yield self.application.db['user'].find_one({"userid":user_openid},{"_id":1,"userid":1,"help_cnt":1})
+            access_token = yield self.application.cache.sget('weixin_api_token') 
+            url = "https://api.weixin.qq.com/cgi-bin/user/info?access_token={0}&openid={1}&lang={2}"
+            url = url.format(access_token,user_openid,"zh_CN")
+            response = yield send_async_request(url)
+            userdata = json.loads(response.body.decode("utf-8"))
+            userdata['createtime'] = userdata.pop('subscribe_time',time.time()) 
+            userdata['userid'] = userdata.pop('openid')
+            userdata['username'] = userdata.pop('nickname')
+            userdata['address'] = ''
+            userdata['location'] = [0, 0]
+            del userdata['remark']
+            del userdata['groupid']
+            del userdata['tagid_list']
+            del userdata['language']
+            if user_existence:
+                userdata['help_cnt'] = user_existence['help_cnt']
+                userdata['_id'] = user_existence['_id']
+            else:
+                userdata['help_cnt'] = {"posted_help_num":0, "done_help_num":0}
+            yield self.application.db['user'].save(userdata)
+            self.application.task.send_task('worker.send_welcome_text', args=(user_openid,))
+        except Exception as e:
+            self.application.task.send_task("worker.send_failed_text", args=(user_openid,))
+            self.write("failed")
+            return 
+        self.write("success")  
 
     @gen.coroutine
     def handle_unsubscribe_event(self):
         user_openid = self.req_msg.get("FromUserName")
         try:
-            yield self.application.db.user.remove({"userid":user_openid})
+            yield self.application.db['user'].update({"userid":user_openid},{"$set":{"subscribe":0}})
         except Exception as e:
-            pass
-        self.write("")
+            self.write("failed")
+            return 
+        self.write("success")
     
     @gen.coroutine
     def handle_click_event(self):
