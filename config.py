786c20fe094aa5fe8dead37e0669cdb2ec44d4b9
john <19941222hb@gmail.com>
2016-10-06 16:45:14 +0800

    format config file

--- config.py
+++ config.py
@@ -4,11 +4,11 @@ config = {
 	"public_secret":"4e023cf984e2c4f08afece67e22b1206",
 	"token_url":"https://api.weixin.qq.com/cgi-bin/token",
 	"mongodb":{
-         	"host": "127.0.0.1",
-		"port": 27017,
-         },
+        "host": "127.0.0.1",
+    	"port": 27017,
+    },
 	"redis":{
-		"host": "127.0.0.1",
-		"port": 6379,
-         }
+	    "host": "127.0.0.1",
+	    "port": 6379,
+    }
 }
