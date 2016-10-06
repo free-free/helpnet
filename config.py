cb4787134ac409ae57e61c33dae58bcea83ed71a
whoami <19941222hb@gmail.com>
2016-10-06 16:22:35 +0800

    add app config file

---
+++ config.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3.5
+config = {
+	"public_appid":"wxdfd14622e7cf37bc",
+	"public_secret":"4e023cf984e2c4f08afece67e22b1206",
+	"token_url":"https://api.weixin.qq.com/cgi-bin/token",
+	"mongodb":{
+         	"host": "127.0.0.1",
+		"port": 27017,
+         },
+	"redis":{
+		"host": "127.0.0.1",
+		"port": 6379,
+         }
+}
